9c2d94af1fece5a3ab85c26ea4dfeca28f0b8c78
James Montemagno <james.montemagno@gmail.com>
2014-09-30 23:32:25 +0200

    Initial Comit

---
+++ AarhusWeather/AarhusWeather/WeatherPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AarhusWeather
+{
+  public partial class WeatherPage
+  {
+    public WeatherPage()
+    {
+      InitializeComponent();
+      BindingContext = new WeatherViewModel();
+
+      this.SetBinding(Page.IsBusyProperty, "IsBusy");
+      
+    }
+  }
+}
