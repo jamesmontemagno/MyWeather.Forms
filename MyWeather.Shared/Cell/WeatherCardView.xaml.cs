bfea8cc13480eed67a01ad5e56e3c29982f7f61b
James Montemagno <james.montemagno@gmail.com>
2018-05-16 13:22:55 -0700

    Add some css and flex

---
+++ MyWeather.Shared/Cell/WeatherCardView.xaml.cs
@@ -0,0 +1,38 @@
+﻿using MyWeather.Models;
+using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MyWeather.Cell
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class WeatherCardView : ContentView
+	{
+		public WeatherCardView ()
+		{
+			InitializeComponent ();
+
+            if(DesignMode.IsDesignModeEnabled)
+            {
+                BindingContext = new WeatherRoot
+                {
+                    Date = DateTime.Now.ToString(),
+                    MainWeather = new Main
+                    {
+                        Temperature = 70
+                    },
+                    Weather = new List<Weather>()
+                            {
+                                new Weather
+                                {
+                                    Icon = "03d",
+                                    Main = "Clear"
+                                }
+                            }
+                };
+            }
+		}
+	}
+}
